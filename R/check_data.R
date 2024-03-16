cf585aff4ad1b280e9b9b7c78b8df858503d5c7e
Danny Squire <15035820+dsquire@users.noreply.github.com>
2024-03-16 14:03:53 -0500

    Update check_data.R

--- R/check_data.R
+++ R/check_data.R
@@ -29,6 +29,26 @@ check_data <- function (data) {
 
   # ignore <- unique(c(ignore, empty_cols, character_cols, nzv_cols))
 
+  <<<<<<< HEAD
+  # This check must be last so we can exclude all the potentially
+  # columns before checking the dimension ratio
+
+  if (dimension_ratio(data, ignore) >= 1) {
+    message("Your ratio of columns to rows is greater than or equal to 1")
+  }
+  =======
+    # if (dimension_ratio(data, ignore) >= 1) {
+    #   message("Your ratio of columns to rows is greater than or equal to 1")
+    # }
+    >>>>>>> d4ef5ef69d8901af52b8be89f97f0cabb7271941
+
+  checklist <- list(
+    empty_rows = empty_rows,
+    empty_cols = empty_cols,
+    character_cols = character_cols,
+    nzv_cols = nzv_cols
+  )
+
   return(checklist)
 
 }
