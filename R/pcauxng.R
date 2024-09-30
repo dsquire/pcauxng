3f1b00d157d448ac1f5e37fd6688c47b657a236c
Danny Squire <15035820+dsquire@users.noreply.github.com>
2024-09-30 13:24:50 -0500

    refactor: Argument Checks
    Added more function argument checks including miceargs1, miceargs2, prcompargs.

--- R/pcauxng.R
+++ R/pcauxng.R
@@ -3,23 +3,24 @@ pcauxng <-
            miceargs1 = NULL,
            miceargs2 = NULL,
            prcompargs = NULL,
-
-           # ignore = NULL,
-           # quiet = FALSE) {
+           ignore = NULL,
+           quiet = FALSE) {
     stopifnot(
       'argument "data" is missing, with no default' = !missing(data),
-      'argument "data" must be a data frame' = is.data.frame(data)
-      # TODO: Add checks for miceargs and prcompargs
+      'argument "data" must be a data frame' = is.data.frame(data),
+      'argument "miceargs1" must be a list' = is.list(miceargs1),
+      'argument "miceargs2" must be a list' = is.list(miceargs2),
+      'argument "prcompargs" must be a list' = is.list(prcompargs)
     )
 
     # TODO: generate raw data with component scores and write out
 
-     original_columns <- colnames(data)
+    original_columns <- colnames(data)
 
     modifyList()
 
     ## TODO: Check for columns with no data and add list to ignore
-    if (ignore != NULL) {
+    if (!is.null(ignore)) {
       ignoredata <- datawizard::get_columns(data, ignore)
       data <- datawizard::data_remove(data, select = ignore)
     }
@@ -33,14 +34,11 @@ pcauxng <-
 
     mice1 <- tryCatch({
       do.call(mice::mice, miceargs1)
-    },
-    error = function(cnd) {
+    }, error = function(cnd) {
       message(conditionMessage(cnd))
-    },
-    warning = function(cnd) {
+    }, warning = function(cnd) {
       message(conditionMessage(cnd))
-    } ,
-    finally = {
+    } , finally = {
       # rm(data)
     })
 
