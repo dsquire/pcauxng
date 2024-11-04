730c1e7e6e713802eba739be83b29deb053327a0
Danny Squire <15035820+dsquire@users.noreply.github.com>
2024-11-04 17:45:10 -0600

    feat: Added new function remove_empty_rows
    New function to find and remove empty rows in a data frame

--- R/remove_empty_rows.R
+++ R/remove_empty_rows.R
@@ -1,7 +1,7 @@
-empty_rows <- function(data) {
+remove_empty_rows <- function(data) {
   stopifnot(
     'argument "data" is missing, with no default' = !missing(data),
     'argument "data" must be a data frame' = is.data.frame(data)
   )
-  as.numeric(row.names(data[rowSums(is.na(data)) == ncol(data), ]))
+  data[rowSums(is.na(data)) != ncol(data), ]
 }
