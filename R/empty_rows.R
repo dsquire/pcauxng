935bea48711f9e3b3584693cbfd9ae4df3915b8c
Danny Squire <15035820+dsquire@users.noreply.github.com>
2024-11-03 19:12:16 -0600

    feat: added new function to find empty rows
    Added empty_rows to find empty rows in a data set. Updated check_data to use this new function and remove dependency on datawizard.

---
+++ R/empty_rows.R
@@ -0,0 +1,8 @@
+
+empty_rows <- function(data) {
+  stopifnot(
+    'argument "data" is missing, with no default' = !missing(data),
+    'argument "data" must be a data frame' = is.data.frame(data)
+  )
+  as.numeric(row.names(data[rowSums(is.na(data)) == ncol(data), ]))
+}
